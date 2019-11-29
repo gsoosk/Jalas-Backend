07d9ea3541db4552e81d554eb35b2da64e1bb0ed
Ssadaf <s.sadaf2277@gmail.com>
2019-11-29 12:08:06 +0330

    Add meeting models

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meeting
+from .models import Room
+from .models import Participant
+
+admin.site.register(Meeting)
+admin.site.register(Room)
+admin.site.register(Participant)
--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    roomName = models.CharField(max_length=100)
+    capacity = models.IntegerField(default=0)
+    location = models.CharField(max_length=200)
+    hasVideoProjector = models.BooleanField(default=False)
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=100)
+    startDateTime = models.DateTimeField('Start time')
+    endDateTime = models.DateTimeField('End time')
+    room = models.ForeignKey('Room', null=True, on_delete=models.SET_NULL)
+    participants = models.ManyToManyField('Participant')
+
