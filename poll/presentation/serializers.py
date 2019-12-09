c19486a89ab2105fe90883fde69aebfa3dbaf5bf
gsoosk <farzadhabibii98@gmail.com>
2019-12-09 15:03:28 +0330

    Add poll serializer

--- poll/presentation/serializers.py
+++ poll/presentation/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from poll.models import MeetingPoll
+from poll.models import PollTime
+from meetings.presentation.serializers import ParticipantSerializer
 
 
 class MeetingPollSerializer(serializers.Serializer):
@@ -17,3 +20,17 @@ class PollChoiceItemSerializer(serializers.Serializer):
     start_time = serializers.DateTimeField()
     end_time = serializers.DateTimeField()
     voters = ParticipantSerializer(many=True)
+
+
+class PollTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollTime
+        fields =['start_date_time', 'end_date_time']
+
+
+class PollSerializer(serializers.ModelSerializer):
+    times = PollTimeSerializer(many=True)
+    creator = ParticipantSerializer(many=True)
+    class Meta:
+        model = MeetingPoll
+        fields = ['title', 'times', 'creator']
